f9fcf5b7c501c40fc84c69154f40cbae8923e9d7
Timur <Kudashev>
2021-09-05 22:55:04 +0300

    Added playing from saved position

--- GameController.h
+++ GameController.h
@@ -3,14 +3,15 @@
 
 class GameController {
 public:
+    GameController();
+    void play_game();
+private:
     Board board;
     History history;
     UserIntelligence ui;
     ArtificialIntelligence ai;
     Color user_color;
-    GameController();
-    void play_game();
-    // void end_game();
+    bool is_white_first_to_move;
     bool is_mate(const Intelligence &intelligence);
     bool is_stalemate(const Intelligence &intelligence);
     void print_board();
